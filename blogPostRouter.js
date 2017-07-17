aad3284b637c10d45f0eacb7b3354331ca2fe964
mikebcbc <mikebconstantino@gmail.com>
2017-07-17 19:03:37 -0400

    Implement PUT

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -49,6 +49,29 @@ router.post('/', (req, res) => {
   });
 });
 
+router.put('/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    res.status(400).json({message: message});
+  }
+
+  const update = {};
+  const updateable = ['title', 'content', 'author'];
+
+  updateable.forEach(field => {
+  	if (field in req.body) {
+  		update[field] = req.body[field];
+  	}
+  });
+
+  Post.findByIdAndUpdate(req.params.id, {$set: update}).exec()
+  .then(post => res.status(204).end())
+  .catch(err => res.status(500).json({message: 'Internal server error'}));
+})
+
 const {Post} = require('./models');
 
 
