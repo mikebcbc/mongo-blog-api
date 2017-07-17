f47596aca533b4f61112d5032479dbf2319e42aa
mikebcbc <mikebconstantino@gmail.com>
2017-07-17 18:55:32 -0400

    Implement POST

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -11,11 +11,42 @@ router.get('/', (req, res) => {
 				posts: posts.map((post) => post.apiRepr())
 			});
 		})
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal Server Error'});
+		});
 });
 
 router.get('/:id', (req, res) => {
 	Post.findById(req.params.id).exec()
-		.then(post => res.json(post.apiRepr()));
+		.then(post => res.json(post.apiRepr()))
+		.catch(err => {
+			console.error(err);
+			res.status(500).json({message: 'Internal Server Error'});
+		});
+});
+
+router.post('/', (req, res) => {
+	const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Post.create({
+  	title: req.body.title,
+  	content: req.body.content,
+  	author: req.body.author
+  })
+  .then(post => res.status(201).json(post.apiRepr()))
+  .catch(err => {
+  	console.error(err);
+  	res.status(500).json({message: 'Internal server error'});
+  });
 });
 
 const {Post} = require('./models');
