e901b5c12608d2cf021d7820003e7ff4e13921f7
mikebcbc <mikebconstantino@gmail.com>
2017-07-19 12:49:54 -0400

    generate and seed data functions

--- tests/test-blog-post.js
+++ tests/test-blog-post.js
@@ -10,3 +10,34 @@ const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+
+function seedData() {
+	console.info('inserting data into db');
+	const data = [];
+	for(let i=1; i<10, i++) {
+		data.push(generateData());
+	}
+	return Post.insertMany(data);
+}
+
+function generateData() {
+	return {
+		title: faker.lorem.sentence(),
+		content: faker.lorem.paragraph(),
+		author: {
+			firstName: faker.name.firstName(),
+			lastName: faker.name.lastName()
+		}
+	}
+}
+
+describe('Blog API', function() {
+	
+	before(function() {
+		return runServer(TEST_DATABASE_URL);
+	})
+
+	beforeEach(function(), {
+		return seedData();
+	})
+})
